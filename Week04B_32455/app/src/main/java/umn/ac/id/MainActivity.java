8631fa40edde909348d4a05df2e09e765d420ded
Farrel Irsyad Fanny <farrel.irsyad@student.umn.ac.id>
2021-02-22 23:43:43 +0700

    Kumpul Mingguan (Week 4)

---
+++ Week04B_32455/app/src/main/java/umn/ac/id/MainActivity.java
@@ -0,0 +1,35 @@
+package umn.ac.id;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button      btnActivity2, btnActivity3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnActivity2 = findViewById(R.id.main_button_change_1);
+        btnActivity3 = findViewById(R.id.main_button_change_2);
+
+        btnActivity2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnActivity3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
